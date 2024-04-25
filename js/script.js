ebf462bdc0753529367894bc790494b1c7203ca0
Jonathan Arriaza <jaarriaza.e@gmail.com>
2024-04-24 23:32:06 -0400

    arreglo modal

--- js/script.js
+++ js/script.js
@@ -19,21 +19,48 @@ function closeMenu() {
     // document.getElementById("menu").style.transform = "0"
 }
 
-var num
-var menu = document.getElementById("menu1")
-var modal = document.getElementById("myModal")
-var span = document.getElementsByClassName("close")[0]
+// var menu = document.getElementById("menu1")
+// var modal = document.getElementById("myModal")
+// var span = document.getElementsByClassName("close")[0]
 
-menu.onclick = function (event) {
-    modal.style.display = "flex";
+// menu.onclick = function (event) {
+//     modal.style.display = "flex";
+// }
+
+// span.onclick = function (event) {
+//         modal.style.display = "none";
+//     }
+
+// window.onclick = function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none"
+//     }
+// }
+
+function openModal(item) {
+    const modal = document.getElementById("myModal")
+    modal.style.display = "block"
+    const modalImg = document.getElementById("modalImg")
+    const modalVideo = document.getElementById("modalVideo")
+    const modalImagen = document.getElementById("modalImagen")
+    modalImg.style.display = "none";
+    modalVideo.style.display = "none";
+    if (item === 'quees' || item === 'estudio' || item === 'valores' || item === 'ustedes' || item === 'cuidados' || item === 'info' || item === 'promos' || item === 'concursos') {
+        modalImg.style.display = "block"
+        modalImg.src = `./media/ico-menu/${item}.webp`
+        modalVideo.style.display = "block"
+        modalVideo.src = `./media/sofi/${item}1.mp4`
+        modalImagen.style.display = "block"
+        modalImagen.src = `./media/sofi/${item}1.webp`  
+    } else {
+    }
 }
 
-span.onclick = function (event) {
-        modal.style.display = "none";
-    }
-
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none"
-    }
+function closeModal() {
+    const modal = document.getElementById("myModal");
+    modal.style.display = "none";
+    const modalImg = document.getElementById("modalImg");
+    const modalVideo = document.getElementById("modalVideo");
+    modalImg.src = "";
+    modalVideo.src = "";
 }
